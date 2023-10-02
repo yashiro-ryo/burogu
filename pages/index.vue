26054a350410eed01b1dd778e9d843160b19748e
yashiroryo <>
2023-10-02 23:02:48 +0900

    feat: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">Main Page</div>
+</template>
+
+<style lang="scss">
+@import "@/public/reset.scss";
+
+:root {
+  --background-color: #fff;
+  --text-color: #000;
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    --background-color: #000;
+    --text-color: #fff;
+  }
+}
+
+.main {
+  background-color: var(--background-color);
+  color: var(--text-color);
+}
+</style>
