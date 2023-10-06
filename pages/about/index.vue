effce5b9cc06733c81719725222d3115261e4cf2
yashiroryo <>
2023-10-07 04:12:35 +0900

    feat: プロフィールページを一部実装

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -3,7 +3,17 @@
     <Navbar />
     <div class="container">
       <div class="about-me">
-        <h3>About Me</h3>
+        <h2>About Me</h2>
+        <div class="profile">
+          <div class="profile-image">
+            <img src="/profile.webp" width="300px" />
+          </div>
+          <div class="profile-name">
+            <h2>矢代 涼</h2>
+            <h3>Yashiro Ryo</h3>
+            <p>徳島県徳島市出身</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -16,5 +26,23 @@ import Navbar from "~/components/Navbar.vue";
 <style lang="scss" scoped>
 .about-me {
   margin-top: 60px;
+
+  .profile {
+    .profile-name {
+      padding-left: 20px;
+    }
+  }
+}
+
+@media screen and (min-width: 891px) {
+  .profile {
+    display: flex;
+  }
+}
+
+@media screen and (max-width: 890px) {
+  .profile {
+    display: block;
+  }
 }
 </style>
