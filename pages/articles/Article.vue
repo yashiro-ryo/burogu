d2ca711d2e480c7b60c8eefdc91ac36939119bbc
yashiroryo <>
2023-10-07 02:34:25 +0900

    feat: Articleコンポーネントを追加

---
+++ pages/articles/Article.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLink :href="`/article/${article.articleId}`">
+    <div class="article">
+      <p class="article-title">{{ article.title }}</p>
+      <small class="created-date">created at: {{ article.createdAt }}</small>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  article: {
+    articleId: number;
+    title: string;
+    createdAt: string;
+  };
+}>();
+</script>
+
+<style lang="scss" scoped>
+.article {
+  padding: 10px;
+  margin-bottom: 20px;
+
+  &:hover {
+    background-color: var(--hover-background-color);
+  }
+}
+</style>
---
+++ pages/articles/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="articles-page">
+    <Navbar />
+    <div class="container">
+      <div class="articles">
+        <div class="articles-list">
+          <h3>Articles</h3>
+          <Article
+            v-for="article in articles"
+            :article="article"
+            :key="article.articleId"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue";
+import Article from "./Article.vue";
+import { ref } from "vue";
+
+const articles = ref([
+  {
+    articleId: 1,
+    title: "初ブログ",
+    createdAt: "2023/09/12",
+  },
+  {
+    articleId: 2,
+    title: "オブジェクト指向UIを読んで",
+    createdAt: "2023/10/07",
+  },
+  {
+    articleId: 3,
+    title: "DDDを勉強して",
+    createdAt: "2023/10/01",
+  },
+]);
+</script>
+
+<style lang="scss" scoped>
+#articles-page {
+  background-color: var(--background-color);
+  color: var(--text-color);
+  min-height: 100vh;
+
+  .container {
+    margin-top: 60px;
+  }
+}
+
+.articles-list {
+  margin-top: 60px;
+}
+</style>
