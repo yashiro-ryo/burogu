d63b3f8b190b0d4a3c08a71b250d15b6b1e9160b
yashiroryo <>
2023-10-07 04:06:23 +0900

    feat: worksページを追加

---
+++ pages/works/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <Navbar />
+    <div class="container">
+      <div class="works">
+        <h2>Works</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<style lang="scss" scoped>
+.works {
+  margin-top: 60px;
+}
+</style>
