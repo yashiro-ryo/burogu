e36f989d00a0eeecf1c3fb52f0a7041736de382f
yashiroryo <>
2023-10-07 04:06:44 +0900

    feat: contactページを追加

---
+++ pages/contact/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <Navbar />
+    <div class="container">
+      <div class="contact">
+        <h2>Contact</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  margin-top: 60px;
+}
+</style>
