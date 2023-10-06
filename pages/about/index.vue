246d0c050a9a1b17cad4b247f9dbbad3344eca0b
yashiroryo <>
2023-10-07 02:45:55 +0900

    feat: aboutmeページの雛形を追加

---
+++ pages/about/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <Navbar />
+    <div class="container">
+      <div class="about-me">
+        <h3>About Me</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<style lang="scss" scoped>
+.about-me {
+  margin-top: 60px;
+}
+</style>
