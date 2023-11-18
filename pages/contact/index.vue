2e0f3b35c21414ab204c91d08059bb15e9a15386
yashiroryo <>
2023-11-19 01:45:47 +0900

    feat: contactの追加

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -4,6 +4,12 @@
     <div class="container">
       <div class="contact">
         <h2>Contact</h2>
+        <div>
+          <h3>Mail</h3>
+          <p>yashiro2000185 {at} yahoo.co.jp</p>
+          <h3>Github</h3>
+          <p><a href="https://github.com/yashiro-ryo">@yashiro-ryo</a></p>
+        </div>
       </div>
     </div>
   </div>
